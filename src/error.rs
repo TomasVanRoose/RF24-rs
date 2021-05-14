054f206592bd3e2b710bdb469950dead717b99c5
Tomas Van Roose <tomas.vanroose@gmail.com>
2021-05-14 15:37:37 +0200

    Add uDebug impl for error

--- src/error.rs
+++ src/error.rs
@@ -1,3 +1,6 @@
+#[cfg(feature = "micro-fmt")]
+use ufmt::{uDebug, uWrite, Formatter};
+
 /// Errors that can occur when sending and receiving data.
 #[derive(Copy, Clone, Debug)]
 pub enum TransferError<SPIError, PinError> {
@@ -9,4 +12,19 @@ pub enum TransferError<SPIError, PinError> {
     CommunicationError(u8),
     /// Max retries reached
     MaximumRetries,
+}
+
+#[cfg(feature = "micro-fmt")]
+impl<SPIError, PinError> uDebug for TransferError<SPIError, PinError> {
+    fn fmt<W: ?Sized>(&self, f: &mut Formatter<'_, W>) -> core::result::Result<(), W::Error>
+    where
+        W: uWrite,
+    {
+        match *self {
+            Self::Spi(_) => f.write_str("SPI error"),
+            Self::Pin(_) => f.write_str("Pin error"),
+            Self::CommunicationError(_) => f.write_str("Communication error"),
+            Self::MaximumRetries => f.write_str("SPI error"),
+        }
+    }
 }
